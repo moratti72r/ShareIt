3465663f0a51c301c56e6fc4c290c2f864734cb8
Marat <marrrat1991@yandex.ru>
2023-06-30 22:05:26 +0500

    fix: 3

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -3,15 +3,26 @@ package ru.practicum.shareit.request;
 import lombok.Data;
 import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * TODO Sprint add-item-requests.
  */
 @Data
+@Entity
+@Table(name = "requests", schema = "public")
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "description", nullable = false, length = 1000)
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "requestor_id")
     private User requestor;
     private LocalDateTime created;
 }
